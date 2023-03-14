6bc92652e3114f64f56284d548cd69b50d5ac76d
jliu6184 <125538432+jliu6184@users.noreply.github.com>
2023-03-13 18:08:50 -0700

    ty bailey :)

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,7 +8,7 @@ import './About.css';
 function About() {
     return (
       <div className="About">
-        <h2 class="intro">hi, i'm<span class="pink-text"> jiajun.</span></h2>
+        <h2 class="intro">hi, i'm<span className="pink-text"> jiajun.</span></h2>
         <h3 class="intro-description">i'm a student studying <span class="pink-text">computer science</span> and <span class="pink-text">data science</span> at uc berkeley.</h3>
       </div>
     );
